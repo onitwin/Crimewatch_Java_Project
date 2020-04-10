24b5ca234b9163422393af1eef691c78ed28570b
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-04-10 12:32:56 +0100

    project files

---
+++ backend/Crimewatch_backend/src/main/java/com/example/codeclan/Crimewatch_backend/CrimewatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.Crimewatch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimewatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimewatchBackendApplication.class, args);
+	}
+
+}
