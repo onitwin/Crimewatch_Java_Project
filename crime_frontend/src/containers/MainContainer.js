d02a436c922ba9781e26a00148f73f18e7d06ee1
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-04-10 14:42:14 +0100

    add

---
+++ crime_frontend/src/containers/MainContainer.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NavBar from '../NavBar.js';
+import CrimeContainer from './CrimeContainer'
+import NewsContainer from './NewsContainer'
+
+
+const MainContainer = () => {
+
+  return(
+    <Router>
+    <Fragment>
+    <NavBar/>
+    <Switch>
+       <Route path="/crimes" component={CrimeContainer}/>
+       <Route path="/news" component={NewsContainer}/>
+    </Switch>
+    </Fragement>
+    </Router>
+  )
+}
---
+++ crime_frontend/src/containers/NewsContainer.js
@@ -0,0 +1,32 @@
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react';
+import NewsList from '../components/news/NewsList';
+
+class NewsContainer extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      articles: []
+    }
+  }
+
+  // componentDidMount(){
+  //   const request = new Request();
+  //   request.get('link').then((data) => {this.setState({articles:data})})
+  // }
+
+  render(){
+    return(
+      <p> cats </p>
+    //   <Router>
+    //   <Fragment>
+    //   <Switch>
+    //   <Route render={(props) =>{
+    //   return <NewsList raids={this.state.articles}/>
+    // }} />
+    //   </Switch>
+    //   </Fragment>
+    //   </Router>
+    )
+  }
+}
