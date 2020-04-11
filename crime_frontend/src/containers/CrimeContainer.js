6a0df11d414ca818f4bbbcad569fc26be3b3642e
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-04-11 12:39:21 +0100

    fetch for postcode data

--- crime_frontend/src/containers/CrimeContainer.js
+++ crime_frontend/src/containers/CrimeContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 // import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-// import Request from '../helpers/request'
+
 // import NavBar from '../NavBar';
 
 class CrimeContainer extends Component{
@@ -8,21 +8,27 @@ class CrimeContainer extends Component{
     super(props);
     this.state = {
       crimes: [],
-      postcode: ""
+      postcode: "",
+      postcode_data: []
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.isValidPostcode = this.isValidPostcode.bind(this)
     this.formatPostcode = this.formatPostcode.bind(this)
+
   }
 
-//   componentDidMount(){
-//   const request = new Request();
-//   request.get('api/crimes').then((data) => {
-//     this.setState({crimes: data})
-//   })
-// }
+async  fetchPostcodeData(postcode){
+  const url = "http://api.postcodes.io/postcodes/"+postcode
+   await fetch(url).then((data) => {
+    this.setState({postcode_data: data})
+  })
+
+}
+
+
+
 
 handleChange(event){
 
@@ -39,7 +45,7 @@ handleChange(event){
 formatPostcode(p) {
     if (this.isValidPostcode(p)) {
         var postcodeRegEx = /(^[A-Z]{1,2}[0-9]{1,2})([0-9][A-Z]{2}$)/i;
-        return p.replace(postcodeRegEx,"$1 $2"). toUpperCase();
+        return p.replace(postcodeRegEx,"$1 $2").toUpperCase();
     } else {
       return  alert('An invalid postcode was submited');
 
@@ -49,6 +55,7 @@ formatPostcode(p) {
 handleSubmit(event){
   event.preventDefault();
   this.setState({postcode:this.formatPostcode(this.state.postcode)})
+  this.fetchPostcodeData(this.state.postcode)
 }
 
 
