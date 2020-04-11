434bdd419640a7ba8ea94efaf96416df40b2f151
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-04-11 11:39:58 +0100

    postcode entry

--- crime_frontend/src/containers/CrimeContainer.js
+++ crime_frontend/src/containers/CrimeContainer.js
@@ -7,8 +7,14 @@ class CrimeContainer extends Component{
   constructor(props){
     super(props);
     this.state = {
-      crimes: []
+      crimes: [],
+      postcode: ""
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.isValidPostcode = this.isValidPostcode.bind(this)
+    this.formatPostcode = this.formatPostcode.bind(this)
   }
 
 //   componentDidMount(){
@@ -18,10 +24,46 @@ class CrimeContainer extends Component{
 //   })
 // }
 
+handleChange(event){
+
+  let postcode = this.state.postcode
+  postcode = event.target.value
+  this.setState({postcode:postcode})
+}
+
+ isValidPostcode(p) {
+    var postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i;
+    return postcodeRegEx.test(p);
+}
+
+formatPostcode(p) {
+    if (this.isValidPostcode(p)) {
+        var postcodeRegEx = /(^[A-Z]{1,2}[0-9]{1,2})([0-9][A-Z]{2}$)/i;
+        return p.replace(postcodeRegEx,"$1 $2"). toUpperCase();
+    } else {
+      return  alert('An invalid postcode was submited');
+
+    }
+}
+
+handleSubmit(event){
+  event.preventDefault();
+  this.setState({postcode:this.formatPostcode(this.state.postcode)})
+}
+
+
 render(){
   return(
-
+<div>
  <p> Crime Front Page </p>
+
+<form  onSubmit={this.handleSubmit}>
+  <input type="text" placeholder="Postcode"
+  onChange={this.handleChange} value={this.state.postcode}/>
+
+  <button type="submit"> Submit </button>
+</form>
+</div>
   )
 }
 
