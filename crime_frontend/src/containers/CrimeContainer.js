22087c01351278d19140782f437498fa7324e263
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-04-11 13:24:03 +0100

    fetch crime data

--- crime_frontend/src/containers/CrimeContainer.js
+++ crime_frontend/src/containers/CrimeContainer.js
@@ -7,15 +7,18 @@ class CrimeContainer extends Component{
   constructor(props){
     super(props);
     this.state = {
-      crimes: [],
+      crimes: "",
       postcode: "",
-      postcode_data: []
+      postcode_data: [],
+      date: ""
     }
 
-    this.handleChange = this.handleChange.bind(this)
+    this.handlePostcodeChange = this.handlePostcodeChange.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.isValidPostcode = this.isValidPostcode.bind(this)
     this.formatPostcode = this.formatPostcode.bind(this)
+
 
   }
 
@@ -24,20 +27,32 @@ async  fetchPostcodeData(postcode){
    await fetch(url).then(data => data.json()).then((data) => {
     this.setState({postcode_data: data.result})
   })
+  this.fetchCrimeData(this.state.postcode_data)
+}
 
+fetchCrimeData(postcode){
+  const url = ("https://data.police.uk/api/crimes-at-location?date="+this.state.date+"&lat="+postcode.latitude +"&lng="+postcode.longitude)
+   fetch(url).then(data => data.json()).then((data) =>
+    console.log(data))
 
 }
 
 
 
 
-handleChange(event){
+handlePostcodeChange(event){
 
   let postcode = this.state.postcode
   postcode = event.target.value
   this.setState({postcode:postcode})
 }
 
+handleDateChange(event){
+
+  let date = this.state.date
+  date = event.target.value
+  this.setState({date:date})
+}
  isValidPostcode(p) {
     var postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i;
     return postcodeRegEx.test(p);
@@ -57,6 +72,8 @@ handleSubmit(event){
   event.preventDefault();
   this.setState({postcode:this.formatPostcode(this.state.postcode)})
   this.fetchPostcodeData(this.state.postcode)
+  this.setState({date:this.state.date})
+
 }
 
 
@@ -67,8 +84,8 @@ render(){
 
 <form  onSubmit={this.handleSubmit}>
   <input type="text" placeholder="Postcode"
-  onChange={this.handleChange} value={this.state.postcode}/>
-
+  onChange={this.handlePostcodeChange} value={this.state.postcode}/>
+<input type="month" placeholder="date"   onChange={this.handleDateChange} value={this.state.date}/>
   <button type="submit"> Submit </button>
 </form>
 </div>
